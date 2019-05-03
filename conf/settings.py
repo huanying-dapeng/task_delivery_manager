22b6ff1150480b6f3b50731011da57bcc49f6b56
huanying-dapeng <757049042@qq.com>
2019-05-04 00:22:06 +0800

    add setting

---
+++ conf/settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@time    : 2019/5/3 21:14
+@file    : settings.py
+@author  : zhipeng.zhao
+@contact : 757049042@qq.com
+"""
+from core.common_tools import *
+
+SERVER_HOST = get_host()
+SERVER_PORT = get_port(8888)
