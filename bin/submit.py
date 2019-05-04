c7a27eb3af8706c9b5ff8ea4c1783cea5b189742
huanying-dapeng <757049042@qq.com>
2019-05-05 00:03:02 +0800

    complete the raw version

--- bin/submit.py
+++ bin/submit.py
@@ -9,8 +9,18 @@
 import os
 import sys
 
+bin_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(bin_dir))
+
 from conf import settings
-
 settings.BIN_DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.dirname(settings.BIN_DIR))
 
+from core.params_parser import ParamsParser
+from core.master import Master
+
+
+if __name__ == '__main__':
+    params_obj = ParamsParser()
+    if params_obj.is_ready:
+        maser = Master(params_obj)
+        maser.run()
