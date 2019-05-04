2b3fe9abd4d3430913fadeab3499f743c9586886
huanying-dapeng <757049042@qq.com>
2019-05-04 22:14:45 +0800

    modify client

--- core/client.py
+++ core/client.py
@@ -8,16 +8,14 @@
 """
 
 import zerorpc
-import msgpack
 
 
-class Clint(object):
+class Client(object):
     def __init__(self, endpoint, job_id):
         self.__endpoint = endpoint
         self.__job_id = job_id
-        self._clent = None
-        # self._clent = zerorpc.Client(connect_to=self.__endpoint)
-        self.__statuses = {'running', 'start', 'end'}
+        self._client = None
+        self.__statuses = {'running', 'start', 'end', 'wait'}
 
     def send(self, msg, status='running'):
         if not isinstance(msg, (str, dict, list, tuple)):
@@ -25,19 +23,21 @@ class Clint(object):
         if status not in self.__statuses:
             raise ValueError('status must be running, start or end.')
         msg = {'id': self.__job_id, 'info': msg, 'status': status}
-        return self._clent.add_msg(msg)
+        return self._client.add_msg(msg)
 
     def __enter__(self):
-        self._clent = zerorpc.Client(connect_to=self.__endpoint)
+        self._client = zerorpc.Client(connect_to=self.__endpoint)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_tb is not None:
             raise Exception(str(exc_tb) + ' ' + str(exc_val))
-        self._clent.close()
+        if self._client is not None:
+            self._client.close()
+            self._client = None
 
 
 if __name__ == '__main__':
-    c = Clint('tcp://127.0.0.1:8888', '1')
+    c = Client('tcp://127.0.0.1:8888', '1')
     with c:
-        print(c.send({1:2}))
+        print(c.send({1:2, 'all': True}))
