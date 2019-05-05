22371abcdae7394afc4eb83fa5f4f6c812c2325b
dapeng <757049042@qq.com>
2019-05-05 18:21:22 +0800

    move worker class and manager to worker module

--- bin/cmd_runner.py
+++ bin/cmd_runner.py
@@ -6,58 +6,25 @@
 @author  : zhipeng.zhao
 @contact : 757049042@qq.com
 """
-import os
-import time
-from threading import Thread
 import argparse
+import os
 import sys
-from subprocess import Popen
 
 # add project path to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from core.client import Client
-
-class Command(Thread):
-    def __init__(self, args_obj):
-        super(Command, self).__init__()
-        self.cmd = args_obj.cmd
-        self.endpoint = args_obj.endpoint
-        self.worker_id = args_obj.worker_id
-        self.status = 'wait'  # running, end, error
-        self.name = self.worker_id
-        self.start_time = time.time()
-
-    def run_cmd(self):
-        process = Popen(self.cmd, shell=True, universal_newlines=True, stdout=sys.stdout, stderr=sys.stderr)
-        self.status = 'running'
-        process.wait()
-        sys.stdout.flush()
-        sys.stderr.flush()
-        returncode = process.returncode
-        if returncode == 0:
-            self.status = 'end'
-        else:
-            self.status = 'error'
-
-    def run(self):
-        self.run_cmd()
+from core.worker import Worker, MSGManager
 
 
-class MSGManager(Thread):
-    def __init__(self, cmd_obj: Command):
-        super(MSGManager, self).__init__()
-        self.__cmd_obj = cmd_obj
-        self.__client = Client(cmd_obj.endpoint, cmd_obj.worker_id)
-
-    def run(self):
-        while True:
-            with self.__client as client:
-                msg = {'used_time': round(time.time() - self.__cmd_obj.start_time, 5)}
-                client.send(msg, status=self.__cmd_obj.status)
-            if self.__cmd_obj.status in ('end', 'error'):
-                break
-            time.sleep(15)
+def run(args_obj):
+    # create worker thread
+    cmd_obj = Worker(args_obj=args_obj)
+    # create msg manager thread
+    msg_obj = MSGManager(cmd_obj)
+    msg_obj.start()
+    cmd_obj.start()
+    cmd_obj.join()
+    msg_obj.join()
 
 
 if __name__ == '__main__':
@@ -69,11 +36,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 7:
         parser.parse_args(['-h'])
 
-    args_obj = parser.parse_args()
-    print(os.path.dirname(os.path.dirname(__file__)))
-    cmd_obj = Command(args_obj=args_obj)
-    msg_obj = MSGManager(cmd_obj)
-    msg_obj.start()
-    cmd_obj.start()
-    cmd_obj.join()
-    msg_obj.join()
+    run(parser.parse_args())
